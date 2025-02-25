2e13d849d2c7a573cfed3d0db2830f5a1dec414f
Mari Montoya <kulafumon@gmail.com>
2025-02-25 14:48:11 -0500

    Made it better.

--- main.py
+++ main.py
@@ -16,41 +16,41 @@ if __name__ == "__main__":
     # Comment this if you wanna just work on previous data
     articles_df : pd.DataFrame = pd.read_csv(TOP_25_FILE)
 
-    # Make matrix of documents and words used for all articles
-    count_vectorizer = CountVectorizer()
-    document_terms_matrix = count_vectorizer.fit_transform(articles_df['text'])
-    document_terms_matrix = document_terms_matrix.toarray()
-
+    # Make matrix of articles and words used for all articles
+    count_vectorizer = CountVectorizer(stop_words='english')
+    article_terms_matrix = count_vectorizer.fit_transform(articles_df['text'])
+    article_terms_matrix = article_terms_matrix.toarray()
     # Make matrix of term frequency and inverse document frequency
-    tf_idf_vectorizer = TfidfVectorizer()
+    tf_idf_vectorizer = TfidfVectorizer(stop_words='english')
     tf_idf_matrix = tf_idf_vectorizer.fit_transform(articles_df['text'])
     tf_idf_matrix = tf_idf_matrix.toarray()
 
     # Word frequencies
     # each article
     for i in range(25):
-        word_freq = document_terms_matrix[i]
+        word_freq = article_terms_matrix[i]
         word_freq_df = pd.DataFrame({'word': count_vectorizer.get_feature_names_out(), 'frequency': word_freq})
-        word_freq_df.sort_values(['frequency', 'word'], ascending=False, inplace=True)
+        word_freq_df.sort_values(by=['frequency', 'word'], ascending=False, inplace=True)
         print(f'Word Frequencies of Article: {articles_df.loc[i, 'name']}')
         print(word_freq_df.head(10))
-
     # all articles combined
-    word_freq = np.sum(document_terms_matrix, axis=0)
+    word_freq = np.sum(article_terms_matrix, axis=0)
     word_freq_df = pd.DataFrame({'word': count_vectorizer.get_feature_names_out(), 'frequency': word_freq})
-    word_freq_df.sort_values(['frequency', 'word'], ascending=False, inplace=True)
+    word_freq_df.sort_values(by=['frequency', 'word'], ascending=False, inplace=True)
     print('Word Frequencies of All Articles:')
     print(word_freq_df.head(10))
 
-    # Document similarity
-    print('Document Similarities:')
+    # Article similarity
+    print('Article Similarities:')
     article_similarities = cosine_similarity(tf_idf_matrix)
     for i in range(25):
         most_similar = np.argsort(article_similarities[i])[-2] # the last one is 1, which is similarity with self
         print(f'Article most similar to {articles_df.loc[i, 'name']} is {articles_df.loc[most_similar, 'name']} with score of: {article_similarities[i][most_similar]}')
+        average_similarity = np.mean(np.sort(article_similarities[i])[:len(article_similarities)])
+        print(f'The average similarity score is: {average_similarity}')
 
     # Grouping words by topic
-    nmf_model = NMF(n_components=25, random_state=100)
+    nmf_model = NMF(n_components=2, random_state=100)
     topic_matrix = nmf_model.fit_transform(tf_idf_matrix)
     # Get top words for each topic
     feature_names = tf_idf_vectorizer.get_feature_names_out()
